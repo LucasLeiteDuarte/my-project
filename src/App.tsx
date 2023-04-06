cd17e11616427ee03e8548c7e24b696b303b00c5
Lucas <lucasleitet@gmail.com>
2023-04-06 20:00:26 -0300

    reactHookForm+zod

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import "./styles/global.css";
 
+import {useForm} from 'react-hook-form'
+
+
 export function App() {
+  const {register} = useForm()
+  /*  o useForm ira devolver duas informaçães que são cruciais.
+   A primeira delas é (register){que vai falar para o react hook form,
+     quais são os campos do nosso formulário, então, vamos registrar quais 
+    são os campos do formulário }. 
+  */
   return (
     <main className="h-screen bg-zinc-50 flex items-center justify-center">
       <form className="flex flex-col gap-4 w-full max-w-xs">
         <div className="flex flex-col gap-1">
           <label htmlFor="">E-mail</label>
           <input
-          placeholder="E-mail"
+            placeholder="E-mail"
             type="email"
             name="email"
             className="border border-zinc-200 shadow-sm rounded h-10 px-3"
